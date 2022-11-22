d8562ab860f8ee206693dfb10e8ef7a1321f409e
Singgih Pratama <78544446+mastama@users.noreply.github.com>
2022-11-22 11:57:16 +0700

    get table to json on web, setting router

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"net/http"
 	"patungan-be/entity"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
@@ -12,21 +14,39 @@ import (
 
 func main() {
 	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	// dsn := "root:tams4596@tcp(127.0.0.1:3306)/patungan?charset=utf8mb4&parseTime=True&loc=Local"
+	// db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
+	// fmt.Println("Connection to database is success!")
+
+	// var users []entity.User
+	// db.Find(&users)
+
+	// // untuk menampilkan data
+	// for _, user := range users {
+	// 	fmt.Println(user.Name)
+	// 	fmt.Println(user.Email)
+	// 	fmt.Println("====================")
+	// }
+	router := gin.Default()
+	router.GET("/handler", handler)
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:tams4596@tcp(127.0.0.1:3306)/patungan?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	fmt.Println("Connection to database is success!")
+	fmt.Println("Connection to database is success")
 
 	var users []entity.User
 	db.Find(&users)
 
-	// untuk menampilkan data
-	for _, user := range users {
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-		fmt.Println("====================")
-	}
+	c.JSON(http.StatusOK, users)
 }
